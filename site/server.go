1855e71ada611601b5bda4583e877ec454eb8d16
ShrwdFlrst <shrwdflrst@gmail.com>
2017-09-06 08:58:12 +0100

    REFACTOR change repo name

--- site/server.go
+++ site/server.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
-	"github.com/shrwdflrst/cocktailbot/err"
-	"github.com/shrwdflrst/cocktailbot/search"
-	"github.com/shrwdflrst/cocktailbot/site/controllers"
+	"github.com/cocktailbot/app/site/controllers"
 )
 
-func apiRecipes(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-
-	w.Header().Set("Content-Type", "application/json")
-	results, e := search.ByIngredient(query["ingredients"], 0, 10)
-	err.Check(e)
-	json.NewEncoder(w).Encode(results)
-}
-
 func main() {
-	http.HandleFunc("/api/recipes", apiRecipes)
-
 	fs := http.FileServer(http.Dir(controllers.Prefix + "static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
